d8d331b6e14b4545c072f9d306ab8c5f5ce109e0
wellerson-pinheiros <wellersonpinheiros@outlook.com>
2024-04-06 21:54:16 -0300

    pequena alteraçao na linha de imgs

--- verificadordeidade/javascript.js
+++ verificadordeidade/javascript.js
@@ -1,13 +1,13 @@
 function calcular(){
     let idade = window.document.getElementById('idade').value
     let cchekbox = window.document.getElementsByClassName('genero')
-    let imag = window.document.getElementById('imgs')
+    let imgs = window.document.getElementById('imgs')
     let resultado = window.document.getElementById('res')
-            if (idade == 0 ){
+            if (idade == 0){
                 window.alert('erro')
             } else if( idade <=10) {
                 res.innerHTML = 'vc é uma criança'
-                 imag.src = 'imagens/crianca-masc.png' 
+                 img.src = 'imagens/crianca-masc.png' 
 
             }
 
