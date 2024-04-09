026798458a7629d2c3d300fd02180defd3bd8d35
wellerson-pinheiros <wellersonpinheiros@outlook.com>
2024-04-08 23:28:55 -0300

    alteraçao na logica do codigo
    mudei a logica do codigo e alguns aspectos

--- verificadordeidade/javascript.js
+++ verificadordeidade/javascript.js
@@ -1,14 +1,33 @@
 function calcular(){
-    let idade = window.document.getElementById('idade').value
-    let cchekbox = window.document.getElementsByClassName('genero')
-    let imgs = window.document.getElementById('imgs')
-    let resultado = window.document.getElementById('res')
-            if (idade == 0){
-                window.alert('erro')
-            } else if( idade <=10) {
-                res.innerHTML = 'vc é uma criança'
-                 img.src = 'imagens/crianca-masc.png' 
+    let data = new Date()
+    let horas = data.getFullYear()
+    let nasc = window.document.getElementById('idade')
+    let res = window.document.getElementById('res')
+    let img = window.document.getElementById('imgs')
 
-            }
+            if(nasc.length == 0 || Number(nasc.value) > horas || nasc.value < 1950){
 
-}   
+                 window.alert('erro por favor coloque a data de nascimento')
+            } else {
+
+                   let sex = window.document.getElementsByName('sex')
+                   let idade = horas - Number(nasc.value) 
+                   let genero = ''
+                    if(sex[0].checked ){
+                    genero = 'homem'
+                    if(idade >=0 && idade <10){
+                            img.src = 'imagens/crianca-masc.png'
+                    }else if (idade >=11 && idade <= 26){
+                            img.src = 'imagens/adolescente-masc.png'
+                    
+                    }else if (idade >26 && idade <= 50){
+                            img.src='imagens/adulto-masc.png'                
+             
+                    } else if (idade > 50 && idade <= 107 ){
+                                img.src='imagens/idoso-masc.png'
+                    }
+                    else if (sex[1].checked){
+                            genero ='mulher'
+                    }
+                   res.innerHTML = `detectamos genero ${genero} com ${idade} anos`
+              }  }}
